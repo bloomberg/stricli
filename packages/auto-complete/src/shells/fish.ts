63f12e6fe4282969b8a99475722d80dcb7d82023
loks0n <22452787+loks0n@users.noreply.github.com>
2025-02-12 10:34:37 +0000

    fix: types
    Signed-off-by: loks0n <22452787+loks0n@users.noreply.github.com>

--- packages/auto-complete/src/shells/fish.ts
+++ packages/auto-complete/src/shells/fish.ts
@@ -10,13 +10,10 @@ function getFilePath(targetCommand: string, completionsDir: string): string {
     return nodePath.join(completionsDir, `${targetCommand}.fish`);
 }
 
-const BLOCK_START_LINE = (targetCommand: string) =>
-    `# @stricli/auto-complete START [${targetCommand}]`;
+const BLOCK_START_LINE = (targetCommand: string) => `# @stricli/auto-complete START [${targetCommand}]`;
 const BLOCK_END_LINE = "# @stricli/auto-complete END";
 
-export async function forFish(
-    context: StricliAutoCompleteContext
-): Promise<ShellManager | undefined> {
+export async function forFish(context: StricliAutoCompleteContext): Promise<ShellManager | undefined> {
     const { os = nodeOs, fs = nodeFs, path = nodePath } = context;
     if (!context.process.env["SHELL"]?.includes("fish")) {
         context.process.stderr.write(`Skipping fish as shell was not detected.\n`);
@@ -27,9 +24,7 @@ export async function forFish(
     try {
         await fs.promises.mkdir(completionsDir, { recursive: true });
     } catch {
-        context.process.stderr.write(
-            `Could not create fish completions directory at ${completionsDir}.\n`
-        );
+        context.process.stderr.write(`Could not create fish completions directory at ${completionsDir}.\n`);
         return;
     }
     return {
@@ -55,12 +50,10 @@ export async function forFish(
             try {
                 await fs.promises.writeFile(filePath, fileContent);
                 context.process.stdout.write(
-                    `Fish completions installed to ${filePath}. Restart fish shell or run 'source ${filePath}' to load changes.\n`
+                    `Fish completions installed to ${filePath}. Restart fish shell or run 'source ${filePath}' to load changes.\n`,
                 );
             } catch {
-                context.process.stderr.write(
-                    `Failed to write fish completions file at ${filePath}.\n`
-                );
+                context.process.stderr.write(`Failed to write fish completions file at ${filePath}.\n`);
             }
         },
         uninstall: async (targetCommand: string) => {
@@ -68,12 +61,10 @@ export async function forFish(
             try {
                 await fs.promises.unlink(filePath);
                 context.process.stdout.write(
-                    `Fish completions removed from ${filePath}. Restart fish shell or run 'source ${filePath}' to update changes.\n`
+                    `Fish completions removed from ${filePath}. Restart fish shell or run 'source ${filePath}' to update changes.\n`,
                 );
             } catch {
-                context.process.stderr.write(
-                    `Could not remove fish completions file at ${filePath}.\n`
-                );
+                context.process.stderr.write(`Could not remove fish completions file at ${filePath}.\n`);
             }
         },
     };
