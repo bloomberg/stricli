7b185c27cf17468570fefc90e20dc3d7ff79a374
mmolisani <mmolisani@bloomberg.net>
2026-01-15 16:38:23 -0500

    fix: enable and fix new eslint rules
    Signed-off-by: mmolisani <mmolisani@bloomberg.net>

--- docs/eslint.config.mjs
+++ docs/eslint.config.mjs
@@ -1,9 +1,24 @@
+import react from "eslint-plugin-react";
+import hooks from "eslint-plugin-react-hooks";
+import a11y from "eslint-plugin-jsx-a11y";
 import ts from "typescript-eslint";
 import common from "../eslint.config.mjs";
 
 export default [
     ...ts.configs.strictTypeChecked,
     ...common,
+    {
+        ...react.configs.flat.recommended,
+        settings: {
+            react: {
+                version: "detect",
+            },
+        },
+    },
+    react.configs.flat["jsx-runtime"],
+    hooks.configs.flat.recommended,
+    a11y.flatConfigs.recommended,
+    a11y.flatConfigs.strict,
     {
         languageOptions: {
             parserOptions: {
