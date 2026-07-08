7ecb7fe9e17696d86d89beb8430cc5e619ac8d39
Michael Molisani <mmolisani@bloomberg.net>
2026-07-08 09:58:05 -0400

    docs: default integrations, relation to app config
    Signed-off-by: Michael Molisani <mmolisani@bloomberg.net>

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -4,6 +4,7 @@
 import { themes as prismThemes } from "prism-react-renderer";
 import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
+import { join } from "node:path";
 
 const config: Config = {
     title: "Stricli",
